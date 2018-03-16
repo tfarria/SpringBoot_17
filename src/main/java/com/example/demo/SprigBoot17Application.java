bd4a779cb825638b5acfa30d012ad5880d635b25
GBTC441019UR <GBTC441019UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-03-16 14:19:24 -0400

    lesson complete

---
+++ src/main/java/com/example/demo/SprigBoot17Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprigBoot17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprigBoot17Application.class, args);
+	}
+}
